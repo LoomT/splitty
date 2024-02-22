a826d8ea75e13228b9ad32bbd7182ba8ba677be6
Kajus Kuncaitis <K.Kuncaitis@student.tudelft.nl>
2024-02-22 18:18:41 +0100

    Added javadocs

--- server/src/test/java/server/api/QuoteControllerTest.java
+++ server/src/test/java/server/api/QuoteControllerTest.java
@@ -62,7 +62,7 @@ public class QuoteControllerTest {
     @Test
     public void databaseIsUsed() {
         sut.add(getQuote("q1"));
-        repo.calledMethods.contains("save");
+        repo.getCalledMethods().contains("save");
     }
 
     private static Quote getQuote(String q) {
