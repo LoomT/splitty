df3bbe4905fef468d4ab6a943a6163a1eab58b38
Bogdan Duminica <B.S.Duminica@student.tudelft.nl>
2024-02-23 18:22:59 +0100

    Modified Expense class

--- commons/src/main/java/commons/Expense.java
+++ commons/src/main/java/commons/Expense.java
@@ -1,9 +1,14 @@
 package commons;
 
 
+import jakarta.persistence.*;
+
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
+@Entity
+@Table(name = "expenses")
 public class Expense {
     /*
     Properties:
@@ -17,14 +22,26 @@ public class Expense {
     List<String> participants for all the people that are splitting the expense
     String type for the type of the current expense
      */
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "expense_id")
     private final int expenseID;
+    @Column(name = "partipant")
     private String participant;
+    @Column(name = "purpose")
     private String purpose;
+    @Column(name = "amount")
     private double amount;
+    @Column(name = "currency")
     private String currency;
+    @Column(name = "date")
+    @Temporal(TemporalType.TIMESTAMP)
     private final Date date;
+    @ElementCollection
+    @CollectionTable(name = "expense_participants", joinColumns = @JoinColumn(name = "expense_id"))
+    @Column(name = "participant")
     private List<String> participants;
+    @Column(name = "type")
     private String type;
 
     /**
@@ -45,7 +62,7 @@ public class Expense {
         this.purpose = purpose;
         this.amount = amount;
         this.currency = currency;
-        this.date = new Date();
+        this.date = date;
         this.participants = participants;
         this.type = type;
     }
@@ -114,4 +131,73 @@ public class Expense {
         return type;
     }
 
+    /**
+     * setter for participant
+     * @param participant
+     */
+    public void setParticipant(String participant) {
+        this.participant = participant;
+    }
+
+    /**
+     * setter for purpose
+     * @param purpose
+     */
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    /**
+     * setter for amount
+     * @param amount
+     */
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    /**
+     * setter for currency
+     * @param currency
+     */
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    /**
+     * setter for type
+     * @param type
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * equals method
+     * @param o
+     * @return true if equals, false otherwise
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Expense expense = (Expense) o;
+        return expenseID == expense.expenseID
+                && Double.compare(expense.amount, amount) == 0
+                && Objects.equals(participant, expense.participant)
+                && Objects.equals(purpose, expense.purpose)
+                && Objects.equals(currency, expense.currency)
+                && Objects.equals(date, expense.date)
+                && Objects.equals(participants, expense.participants)
+                && Objects.equals(type, expense.type);
+    }
+
+    /**
+     * hashCode method
+     * @return an hashCode for a specific object
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(expenseID, participant, purpose,
+                amount, currency, date, participants, type);
+    }
 }
