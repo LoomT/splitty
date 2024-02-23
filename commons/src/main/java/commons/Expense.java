d4fd1a263c92b6bef15148b27e18775873b3cac5
Bogdan Duminica <B.S.Duminica@student.tudelft.nl>
2024-02-23 18:39:58 +0100

    Modified Expense class

--- commons/src/main/java/commons/Expense.java
+++ commons/src/main/java/commons/Expense.java
@@ -19,7 +19,7 @@ public class Expense {
     Date date for the exact date when the expense was paid
     Boolean splitMethod, 0 - equally among all participant, 1 - only a part of them
     List<String> participants for all the people that are splitting the expense
-    String type for the type of the current expense
+    String type for the type of the current created expense
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
