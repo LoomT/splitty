2d0d43ac2d18a575f88dd2cc2008217b0f3a1229
Guney Bayindir <G.Bayindir@student.tudelft.nl>
2024-02-23 15:05:19 +0100

    Solved the pipeline issue and added tags for Expense and Participant classes

--- commons/src/main/java/commons/Expense.java
+++ commons/src/main/java/commons/Expense.java
@@ -1,2 +1,24 @@
-package commons;public class Expense {
+package commons;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private double amount;
+    private String description;
+    private String name;
+    @OneToOne(cascade = CascadeType.PERSIST)
+    private Participant burrower;
+    @OneToOne(cascade = CascadeType.PERSIST)
+    private Participant lender;
+
+    /**
+     * constructor
+     */
+    public Expense(){}
+
 }
