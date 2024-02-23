0035d51d7b03844f2b7ef719230fc20ce4a016ad
Liam Forster <L.J.Forster@student.tudelft.nl>
2024-02-23 15:03:07 +0100

    Implemented a function that generates a uniqueID Added an equals and hashcode method Wrote tests for the equals and hashcode methods.

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -15,7 +15,6 @@ public class Event {
       setter for title
       get, remove, edit and add method for participants
 
-
       */
     private final int eventID;
 
@@ -25,14 +24,15 @@ public class Event {
 
     private final Date creationDate;
 
-    //Constructor:
-    // Takes parameters:
-    //  - int eventID
-    //  - String title
-    //  - List<String> list of participants
-    // returns new Event
-    public Event(int eventID, String title, List<String> participants){
-        this.eventID = eventID;
+    /**
+     * Constructor for an instance of an Event
+     * Takes the following things as parameters:
+     * - Title
+     * - List of participants
+     * - A unique eventID
+     **/
+    public Event(String title, List<String> participants){
+        this.eventID = generateUniqueId();
         this.title = title;
         if(participants == null) this.participants = new ArrayList<String>();
         else this.participants = CopyLtoAL(participants);
@@ -51,6 +51,17 @@ public class Event {
         return this.eventID;
     }
 
+    private static int lastId = 9999;
+
+    public static int generateUniqueId() {
+        lastId = (lastId + 1) % 100000;
+        if (lastId < 10000) {
+            lastId = 10000;
+        }
+        return lastId;
+    }
+
+
     public String getTitle(){
         return this.title;
     }
@@ -61,5 +72,25 @@ public class Event {
 
     public Date getCreationDate() {
         return creationDate;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Event other = (Event) obj;
+        return eventID == other.eventID &&
+                Objects.equals(title, other.title) &&
+                Objects.equals(participants, other.participants) &&
+                Objects.equals(creationDate, other.creationDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(eventID, title, participants, creationDate);
     }
 }
--- commons/src/test/java/commons/EventTest.java
+++ commons/src/test/java/commons/EventTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import java.util.*;
 
 
 public class EventTest {
@@ -11,17 +12,57 @@ public class EventTest {
     @Test
     public void EventGetterTest(){
         String title = "title";
-        Event test = new Event(1, title, null);
-        assertEquals(1, test.getEventID());
+        Event test = new Event(title, null);
         assertEquals("title", test.getTitle());
     }
 
     @Test
     public void EventSetterTest(){
         String title = "title";
-        Event test = new Event(1, title, null);
+        Event test = new Event(title, null);
         assertEquals("title", test.getTitle());
         test.setTitle("newTitle");
         assertEquals("newTitle", test.getTitle());
+    }
+
+    @Test
+    void testSameInstance() {
+        Event event = new Event("Title", List.of("Participant1", "Participant2"));
+        assertEquals(event, event);
+    }
+
+    @Test
+    void testDifferentInstance() {
+        Event event1 = new Event("Title", List.of("Participant1", "Participant2"));
+        Event event2 = new Event("Title", List.of("Participant1", "Participant2"));
+        assertEquals(event1, event2);
+    }
+
+    @Test
+    void testNotEquals() {
+        // This test assumes eventIDs are set differently which should be the case by design
+        Event event1 = new Event("Title", List.of("Participant1"));
+        Event event2 = new Event("Title", List.of("Participant1"));
+        assertNotEquals(event1, event2);
+    }
+
+    @Test
+    void testNull() {
+        Event event = new Event("Title", List.of("Participant1"));
+        assertNotEquals(null, event);
+    }
+
+    @Test
+    void testEqualityDifferentClass() {
+        Event event = new Event("Title", List.of("Participant1"));
+        Object other = new Object();
+        assertNotEquals(event, other);
+    }
+
+    @Test
+    void testHashConsistency() {
+        Event event1 = new Event("Title", List.of("Participant1"));
+        Event event2 = new Event("Title", List.of("Participant1"));
+        assertEquals(event1.hashCode(), event2.hashCode());
     }
 }
