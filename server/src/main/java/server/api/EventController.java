f976468644f5d5ad1a9c539dd1fd247fef6a11f4
Kajus Kuncaitis <K.Kuncaitis@student.tudelft.nl>
2024-02-27 17:47:00 +0100

    More detailed comments, changed event findById

--- server/src/main/java/server/api/EventController.java
+++ server/src/main/java/server/api/EventController.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.database.EventRepository;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/events")
 public class EventController {
@@ -18,25 +20,33 @@ public class EventController {
     }
 
     /**
-     * @param id invite code of event
-     * @return the found event entity or bad request otherwise
+     * Path:
+     * /api/events?id={id}
+     *
+     * @param id invite code of event to search
+     * @return the found event entity or 404 'not found' response otherwise
      */
-    @GetMapping("/{id}")
-    public ResponseEntity<Event> getById(@PathVariable("id") long id) {
-        if (id < 0 || !repo.existsById(id)) {
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.ok(repo.findById(id).get());
+    @GetMapping("")
+    public ResponseEntity<Event> findById(@RequestParam("id") long id) {
+        Optional<Event> event = repo.findById(id);
+        return event.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     /**
      * @param event to save to the database
-     * @return the saved entity
+     * @return the saved entity with an assigned ID
+     * <br><br>
+     * returned JSON example:<br>
+     * {<br>
+     *     "id": 2,<br>
+     *     "title": "test",<br>
+     *     "participants": [],<br>
+     *     "creationDate": "1999-01-01T16:16:58.385+00:00"<br>
+     * }
      */
     @PostMapping(path = { "", "/" })
     public ResponseEntity<Event> add(@RequestBody Event event) {
-
-        if (event.getTitle() == null || event.getTitle().isEmpty()){
+        if (event == null || event.getTitle() == null || event.getTitle().isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
         Event saved = repo.save(event);
