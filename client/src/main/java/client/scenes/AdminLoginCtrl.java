5c327a24e662a08893300a2964d1810d8971602f
Liam Forster <L.J.Forster@student.tudelft.nl>
2024-02-28 21:06:58 +0100

    Created Admin Login controller class

---
+++ client/src/main/java/client/scenes/AdminLoginCtrl.java
@@ -0,0 +1,57 @@
+package client.scenes;
+
+import client.utils.ServerUtils;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AdminLoginCtrl {
+
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private Label passwordLabel;
+
+    @FXML
+    private TextField passwordTextField;
+
+    @FXML
+    private Button loginButton;
+
+    /**
+     * adminLogin screen controller constructor
+     *
+     * @param server utils
+     * @param mainCtrl main scene controller
+     */
+
+    public AdminLoginCtrl(ServerUtils server, MainCtrl mainCtrl) {
+
+        this.server = server;
+        this.mainCtrl = mainCtrl;
+    }
+
+    @FXML
+    private void initialize() {
+        setupListeners();
+    }
+
+    private void setupListeners() {
+        loginButton.setOnAction(event -> login());
+    }
+
+    private void login() {
+        String username = usernameTextField.getText();
+        String password = passwordTextField.getText();
+
+
+    }
+}
