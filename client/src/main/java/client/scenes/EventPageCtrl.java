a815a9ffc770f340743188f7e8d934651bfd167c
Adam Sztanó <sztano@student.tudelft.nl>
2024-03-01 17:23:03 +0100

    eventpage fxml injection

--- client/src/main/java/client/scenes/EventPageCtrl.java
+++ client/src/main/java/client/scenes/EventPageCtrl.java
@@ -1,6 +1,35 @@
 package client.scenes;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.text.Text;
+
 public class EventPageCtrl {
+
+    @FXML
+    private Text eventTitle;
+
+    @FXML
+    private Text participantText;
+
+    @FXML
+    private ChoiceBox<String> participantChoiceBox;
+
+
+    @FXML
+    private void sendInvitesClicked() {
+
+    }
+
+    @FXML
+    private void editParticipantsClicked() {
+
+    }
+    @FXML
+    private void addExpenseClicked() {
+
+    }
+
 
 
 }
