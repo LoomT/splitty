c684ca539c13ad4dfd4139fd847bf252f3cd3029
Thomas Rothfusz <T.G.B.Rothfusz@student.tudelft.nl>
2024-03-03 11:43:47 +0100

    Creating format for testing ParticipantController + fixing some issues

--- server/src/main/java/server/database/ParticipantRepository.java
+++ server/src/main/java/server/database/ParticipantRepository.java
@@ -2,8 +2,6 @@ package server.database;
 
 import commons.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ParticipantRepository extends JpaRepository<Participant, Long> {
 }
---
+++ server/src/test/java/server/api/ParticipantControllerTest.java
@@ -0,0 +1,9 @@
+package server.api;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class ParticipantControllerTest {
+
+    @BeforeEach
+    public void setup(){}
+}
