e0fd06100a52b3c6a9793481d0aaf9fdc6853d1f
Adam Sztanó <sztano@student.tudelft.nl>
2024-03-05 15:22:22 +0100

    Revert "Merge branch 'admin' into 'main'"
    This reverts merge request !18

--- server/src/main/java/server/Main.java
+++ server/src/main/java/server/Main.java
@@ -18,7 +18,6 @@ package server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import static server.AdminService.*;
 
 @SpringBootApplication
 @EntityScan(basePackages = { "commons", "server" })
@@ -31,7 +30,5 @@ public class Main {
      */
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-
-        generateAndStorePassword();
     }
 }
