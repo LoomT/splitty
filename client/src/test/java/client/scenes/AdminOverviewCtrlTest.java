c8ded57bf8a5eddbab7f4d14b68a96971630f869
Kajus Kuncaitis <K.Kuncaitis@student.tudelft.nl>
2024-03-31 20:51:09 +0200

    Got the TestFx to work with AdminOverviewCtrlTest

--- client/src/test/java/client/scenes/AdminOverviewCtrlTest.java
+++ client/src/test/java/client/scenes/AdminOverviewCtrlTest.java
@@ -1,71 +1,109 @@
 package client.scenes;
 
+import client.MyFXML;
 import client.utils.LanguageConf;
 import client.utils.UserConfig;
 import commons.Event;
-import org.junit.jupiter.api.BeforeEach;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
 import utils.TestIO;
 import utils.TestServerUtils;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(ApplicationExtension.class)
 public class AdminOverviewCtrlTest {
     AdminOverviewCtrl ctrl;
-    TestServerUtils server = new TestServerUtils();
-    @BeforeEach
-    void setUp() throws IOException {
-        MainCtrl mainCtrl = new MainCtrl(null);
+    TestServerUtils server;
+    @Start
+    public void start(Stage stage) throws IOException {
+        server = new TestServerUtils();
         UserConfig userConfig = new UserConfig(new TestIO("""
                 serverURL=http://localhost:8080/
                 lang=en
-                recentEventCodes=hello,there"""));
-        ctrl = new AdminOverviewCtrl(server, mainCtrl, userConfig, new LanguageConf(userConfig));
+                recentEventCodes="""));
+        LanguageConf languageConf = new LanguageConf(userConfig);
+        MainCtrl mainCtrl = new MainCtrl(null, languageConf, userConfig);
+        var startScreenLoader = new FXMLLoader(MyFXML.class.getClassLoader().getResource("client/scenes/StartScreen.fxml"),
+                languageConf.getLanguageResources(), null,
+                (type) -> new StartScreenCtrl(server, mainCtrl, languageConf, userConfig, null),
+                StandardCharsets.UTF_8);
+        Parent startScreenParent = startScreenLoader.load();
+        var adminLoginLoader = new FXMLLoader(MyFXML.class.getClassLoader().getResource("client/scenes/AdminLogin.fxml"),
+                languageConf.getLanguageResources(), null,
+                (type) -> new AdminLoginCtrl(server, mainCtrl),
+                StandardCharsets.UTF_8);
+        Parent adminLoginParent = adminLoginLoader.load();
+        var adminOverviewLoader = new FXMLLoader(MyFXML.class.getClassLoader().getResource("client/scenes/AdminOverview.fxml"),
+                languageConf.getLanguageResources(), null,
+                (type) -> new AdminOverviewCtrl(server, mainCtrl, userConfig, languageConf),
+                StandardCharsets.UTF_8);
+        Parent adminOverviewParent = adminOverviewLoader.load();
+        ctrl = adminOverviewLoader.getController();
+        mainCtrl.initialize(stage,
+                new Pair<>(startScreenLoader.getController(), startScreenParent),
+                new Pair<>(null, new StackPane()),
+                new Pair<>(adminLoginLoader.getController(), adminLoginParent),
+                new Pair<>(null, new StackPane()),
+                new Pair<>(ctrl, adminOverviewParent),
+                new Pair<>(null, new StackPane()));
+        mainCtrl.showAdminOverview("password", 500L);
+    }
+
+    @BeforeAll
+    static void setUp() {
+//        System.setProperty("testfx.robot", "glass");
+//        System.setProperty("testfx.headless", "true");
+//        System.setProperty("prism.order", "sw");
+//        System.setProperty("prism.text", "t2k");
+//        System.setProperty("java.awt.headless", "true");
     }
     @Test
     void initPollerTimeOut() throws InterruptedException {
-        ctrl.setPassword("password");
-        ctrl.initPoller(500L);
         Thread.sleep(600);
-        assertTrue(server.getCalls().contains("pollEvents"));
+        assertTrue(server.isPolled());
         assertTrue(server.getStatuses().contains(408));
     }
 
     @Test
     void initPollerResponse() throws InterruptedException {
-        ctrl.setPassword("password");
-        ctrl.initPoller(1000L);
-        Thread.sleep(100);
+        Thread.sleep(200);
         server.createEvent(new Event("title"));
-        Thread.sleep(100);
-        assertTrue(server.getCalls().contains("pollEvents"));
+        Thread.sleep(200);
+        assertTrue(server.isPolled());
         assertTrue(server.getStatuses().contains(204));
     }
 
     @Test
     void initPollerIncorrectPassword() throws InterruptedException {
         ctrl.setPassword("forgor");
-        ctrl.initPoller(500L);
+        Thread.sleep(600);
         server.createEvent(new Event("title"));
         Thread.sleep(600);
-        assertTrue(server.getCalls().contains("pollEvents"));
+        assertTrue(server.isPolled());
         assertTrue(server.getStatuses().contains(401));
         assertFalse(server.getStatuses().contains(204));
     }
 
     @Test
     void stopPoller() throws InterruptedException {
-        ctrl.setPassword("password");
-        ctrl.initPoller(300L);
-        Thread.sleep(500);
         ctrl.stopPoller();
-        Thread.sleep(500);
+        Thread.sleep(700);
         server.createEvent(new Event("title"));
         Thread.sleep(500);
-        assertTrue(server.getCalls().contains("pollEvents"));
+        assertTrue(server.isPolled());
         assertFalse(server.getStatuses().contains(204));
     }
 }
