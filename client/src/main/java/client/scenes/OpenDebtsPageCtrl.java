5bd376ab7a2c37fd4224d68cc5835f260a2a8a4f
Liam Forster <L.J.Forster@student.tudelft.nl>
2024-04-03 18:23:17 +0200

    Filled the OpenDebtsPage with basic components, debt implementation to be finished

--- client/src/main/java/client/scenes/OpenDebtsPageCtrl.java
+++ client/src/main/java/client/scenes/OpenDebtsPageCtrl.java
@@ -2,15 +2,32 @@ package client.scenes;
 
 import client.utils.ServerUtils;
 import commons.Event;
+import commons.Expense;
+import commons.Participant;
 import jakarta.inject.Inject;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class OpenDebtsPageCtrl {
 
     @FXML
     private Button backButton;
+
+    @FXML
+    private PieChart shareChart = new PieChart();
+
+    @FXML
+    private Text totalSumExp;
+
+    @FXML
+    private ScrollPane allDebtsPane;
 
     private Event event;
 
@@ -41,6 +58,34 @@ public class OpenDebtsPageCtrl {
      */
     public void displayOpenDebtsPage(Event event) {
         this.event = event;
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+
+        for(Participant participant : event.getParticipants()) {
+            double sum = 0;
+            for(Expense expense : event.getExpenses()) {
+                for(Participant expenseParticipant : expense.getExpenseParticipants()) {
+                    if(expenseParticipant.getName().equals(participant.getName())) {
+                        sum += expense.getAmount() / expense.getExpenseParticipants().size();
+                    }
+                }
+            }
+            pieChartData.add(new PieChart.Data(participant.getName(), sum));
+        }
+        for (PieChart.Data data : shareChart.getData()) {
+            data.setName(data.getName() + ": " + (int) data.getPieValue() + "%");
+        }
+
+        Font font = new Font("Arial", 12);
+        for (PieChart.Data data : shareChart.getData()) {
+            data.getNode().setStyle("-fx-font-size: " + font.getSize() + "px;");
+        }
+        this.shareChart.setData(pieChartData);
+        this.shareChart.setStyle("-fx-background-color: transparent;");
+        double totalSum = 0;
+        for(Expense expense : event.getExpenses()) {
+            totalSum += expense.getAmount();
+        }
+        totalSumExp.setText("Total sum of all expenses in this event: " + totalSum);
     }
 
 
