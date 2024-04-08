7dc6f03fe13fd7d62dadaea18acdc864d601fafc
Kajus Kuncaitis <K.Kuncaitis@student.tudelft.nl>
2024-04-08 13:33:37 +0200

    Made the CurrencyController be able to fetch historical rates and return a map of rates instead of a string

--- server/src/main/java/server/api/CurrencyController.java
+++ server/src/main/java/server/api/CurrencyController.java
@@ -1,31 +1,52 @@
 package server.api;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import commons.Event;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Currency;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
-public class CurrencyController {   /**
+public class CurrencyController {
+
+    /**
+     * @param date date of format yyyy-mm-dd
      * @return string representation of exchange rates
      */
-    @GetMapping("api/currency")
-    public ResponseEntity<String> get(){
-        HttpClient httpClient = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://openexchangerates.org/api/" + "latest.json?app_id=4368d26633d149e0b992c5bcdce76270")).GET().build();
+    @GetMapping("api/currency/{date}")
+    public ResponseEntity<Map<String, Double>> get(@PathVariable String date){
+        try (HttpClient httpClient = HttpClient.newHttpClient()) {
+            String url = "https://openexchangerates.org/api/historical/";
+            String end = ".json?app_id=4368d26633d149e0b992c5bcdce76270";
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url + date + end)).GET().build();
 
-        HttpResponse response;
-        try {
-            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            if(response.statusCode() != 200){
+                return ResponseEntity.status(response.statusCode()).build();
+            }
+            ObjectReader reader = new ObjectMapper().reader().forType(Map.class);
+            Map<String, Object> map = reader.readValue(response.body());
+            Map<String, Double> rates = (Map<String, Double>) map.get("rates");
+            return ResponseEntity.ok(rates);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
         }
-        return ResponseEntity.ok(response.body().toString());
     }
 }
---
+++ server/src/test/java/server/api/CurrencyControllerTest.java
@@ -0,0 +1,30 @@
+package server.api;
+
+import org.apache.catalina.connector.Response;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrencyControllerTest {
+
+    @Test
+    void get() {
+        CurrencyController currencyController = new CurrencyController();
+        Date date = new Date();
+        String dateString = DateTimeFormatter.ISO_DATE
+                .format(date.toInstant().atZone(ZoneOffset.UTC).toLocalDate());
+        ResponseEntity<Map<String, Double>> response = currencyController.get(dateString);
+        assertNotNull(response);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        Map<String, Double> rates = response.getBody();
+        assertNotNull(rates);
+    }
+}
