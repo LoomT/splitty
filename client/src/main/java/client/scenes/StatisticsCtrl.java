658d7bfc7191f2e96edab456065f68d46f1e83bc
Bogdan Duminica <B.S.Duminica@student.tudelft.nl>
2024-04-09 20:38:22 +0200

    Refactored the code and fixed the format

--- client/src/main/java/client/scenes/StatisticsCtrl.java
+++ client/src/main/java/client/scenes/StatisticsCtrl.java
@@ -3,13 +3,16 @@ package client.scenes;
 import client.MockClass.MainCtrlInterface;
 import client.utils.LanguageConf;
 import client.utils.ServerUtils;
+import client.utils.UserConfig;
 import client.utils.Websocket;
+import client.utils.currency.CurrencyConverter;
 import com.google.inject.Inject;
 import commons.Event;
 import commons.Expense;
 import commons.Tag;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -21,8 +24,13 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
+import java.net.ConnectException;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Currency;
 import java.util.List;
+import java.util.Locale;
 
 public class StatisticsCtrl {
 
@@ -41,6 +49,8 @@ public class StatisticsCtrl {
     private final ServerUtils server;
     private final Websocket websocket;
     private final LanguageConf languageConf;
+    private final CurrencyConverter converter;
+    private final UserConfig userConfig;
 
     private List<Tag> displayedTags = new ArrayList<>();
 
@@ -49,18 +59,24 @@ public class StatisticsCtrl {
      * @param server   server utils instance
      * @param websocket websocket client
      * @param languageConf language config
+     * @param converter currency converter
+     * @param userConfig user config
      */
     @Inject
     public StatisticsCtrl(
             MainCtrlInterface mainCtrl,
             ServerUtils server,
             Websocket websocket,
-            LanguageConf languageConf
+            LanguageConf languageConf,
+            CurrencyConverter converter,
+            UserConfig userConfig
     ) {
         this.mainCtrl = mainCtrl;
         this.server = server;
         this.websocket = websocket;
         this.languageConf = languageConf;
+        this.converter = converter;
+        this.userConfig = userConfig;
     }
 
     /**
@@ -95,7 +111,6 @@ public class StatisticsCtrl {
      * @param event the current event
      */
     public void displayStatisticsPage(Event event) {
-        //pc.getData().clear();
         pc.setLegendVisible(false);
         initPieChart(event);
         initCost(event);
@@ -110,12 +125,29 @@ public class StatisticsCtrl {
      * @return the total cost
      */
     public double initCost(Event event) {
+
         double totalCost = 0;
-        //this does not take into account the currencies
         for (Expense exp : event.getExpenses()) {
-            totalCost += exp.getAmount();
+            double amount = exp.getAmount();
+            try {
+                if(!userConfig.getCurrency().equals("NONE")) {
+                    amount = converter.convert(exp.getCurrency(), userConfig.getCurrency(),
+                            amount, exp.getDate().toInstant());
+                    totalCost += amount;
+                }
+
+            } catch (ConnectException e) {
+                mainCtrl.handleServerNotFound();
+            } catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR,
+                        languageConf.get("Currency.IOError"));
+                alert.setHeaderText(languageConf.get("unexpectedError"));
+                alert.showAndWait();
+            }
         }
-        cost.setText("Total cost: " + totalCost);
+        String preferedCurrency = userConfig.getCurrency();
+        String form = getCurrencySymbol(totalCost, preferedCurrency);
+        cost.setText("Total cost: " + form);
         return totalCost;
     }
 
@@ -125,79 +157,134 @@ public class StatisticsCtrl {
      */
     public void initPieChart(Event event) {
         double totalCost = initCost(event);
-        for (Tag t : event.getTags()) {
-            if (t != null) {
-                double currCost = getAmount(event, t);
-                double percentage = currCost / totalCost * 100;
-                String formattedPercentage = String.format("%.2f", percentage);
-                if (currCost > 0) {
-                    if (!displayedTags.contains(t)) {
-                        displayedTags.add(t);
-                        String temp = "";
-                        temp += t.getName() + "\n";
-                        temp += formattedPercentage + "%";
-                        temp += " (" + currCost + ")";
-
-                        PieChart.Data slice = new PieChart.Data(temp, currCost);
-                        pc.getData().add(slice);
-                        if (slice.getNode() != null) {
-                            Color color = hexToColor(t.getColor());
-                            slice.getNode().setStyle("-fx-pie-color: #" +
-                                    color.toString().substring(2));
-                        }
-                    }
-                }
-            }
-            boolean ok = false;
-            for (PieChart.Data da : pc.getData()) {
-                if (da.getName().contains("No tag")) {
-                    ok = true;
-                }
-            }
-            boolean gasit = false;
-            for (Expense exp : event.getExpenses()) {
-                if (exp.getType() == null) {
-                    gasit = true;
-                }
-            }
-            if (!ok) {
-                if (gasit) {
-                    double costExpensesNoTag = 0;
-                    for (Expense exp : event.getExpenses()) {
-                        if (exp.getType() == null) {
-                            costExpensesNoTag += exp.getAmount();
-                        }
-                    }
-                    String text = configureNoTag(costExpensesNoTag, totalCost);
-                    PieChart.Data slice = new PieChart.Data(text, costExpensesNoTag);
-                    pc.getData().add(slice);
-                    if (slice.getNode() != null) {
-                        slice.getNode().setStyle("-fx-pie-color: #FFFFFF");
-                    }
-                }
-            } else {
-                for (PieChart.Data da : pc.getData()) {
-                    if (da.getName().contains("No tag")) {
-                        double costExpensesNoTag = 0;
-                        for (Expense exp : event.getExpenses()) {
-                            if (exp.getType() == null) {
-                                costExpensesNoTag += exp.getAmount();
-                            }
-                        }
-                        String text = configureNoTag(costExpensesNoTag, totalCost);
-                        da.setName(text);
-                        da.setPieValue(costExpensesNoTag);
-                    }
-                }
-            }
-            for (Expense exp : event.getExpenses()) {
-                if (exp.getType() == null) {
-
-                }
-            }
-
-        }
+        updateTagsPieChart(event, totalCost);
+        updateNoTagPieChart(event, totalCost);
         populateLegend();
+    }
+
+    /**
+     * see which tags have associated expenses
+     * @param event the current event
+     * @param totalCost the total cost of the event
+     */
+    private void updateTagsPieChart(Event event, double totalCost) {
+        for (Tag tag : event.getTags()) {
+            if (tag != null) {
+                double currCost = getAmount(event, tag);
+                if (currCost > 0) {
+                    updateOrAddTagSlice(tag, currCost, totalCost);
+                }
+            }
+        }
+    }
+
+    /**
+     * update or add new slices
+     * @param tag the current tag
+     * @param currCost the current cost
+     * @param totalCost the total cost of the event
+     */
+    private void updateOrAddTagSlice(Tag tag, double currCost, double totalCost) {
+        double percentage = currCost / totalCost * 100;
+        String preferedCurrency = userConfig.getCurrency();
+        String form = getCurrencySymbol(currCost, preferedCurrency);
+        String formattedPercentage = String.format("%.2f", percentage);
+        String tagInfo = tag.getName() + "\n" + formattedPercentage + "% (" + form + ")";
+
+        boolean found = false;
+        for (PieChart.Data slice : pc.getData()) {
+            if (slice.getName().startsWith(tag.getName())) {
+                slice.setName(tagInfo);
+                slice.setPieValue(currCost);
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            PieChart.Data newSlice = new PieChart.Data(tagInfo, currCost);
+            pc.getData().add(newSlice);
+            applyTagColor(newSlice, tag.getColor());
+        }
+    }
+
+    /**
+     * apply color to tag
+     * @param slice the slice
+     * @param colorHex the color string
+     */
+    private void applyTagColor(PieChart.Data slice, String colorHex) {
+        if (slice.getNode() != null) {
+            Color color = hexToColor(colorHex);
+            slice.getNode().setStyle("-fx-pie-color: #" + color.toString().substring(2));
+        }
+    }
+
+    /**
+     * update the no tag slice
+     * @param event the current event
+     * @param totalCost the total cost of the event
+     */
+    private void updateNoTagPieChart(Event event, double totalCost) {
+        boolean hasNoTagSlice = pc.getData().stream().anyMatch(slice ->
+                slice.getName().contains("No tag"));
+        if (!hasNoTagSlice && containsExpensesWithNoTag(event)) {
+            double costExpensesNoTag = calculateExpensesNoTag(event);
+            String text = configureNoTag(costExpensesNoTag, totalCost);
+            PieChart.Data slice = new PieChart.Data(text, costExpensesNoTag);
+            pc.getData().add(slice);
+            slice.getNode().setStyle("-fx-pie-color: #FFFFFF");
+        } else {
+            updateNoTagSlice(event, totalCost);
+        }
+    }
+
+    /**
+     * checks if there are expenses with no tags
+     * @param event the current event
+     * @return true if they are, false otherwise
+     */
+    private boolean containsExpensesWithNoTag(Event event) {
+        return event.getExpenses().stream().anyMatch(expense -> expense.getType() == null);
+    }
+
+    /**
+     * calculate the cost of the no tag expenses
+     * @param event the current event
+     * @return the amount
+     */
+    private double calculateExpensesNoTag(Event event) {
+        double costExpensesNoTag = 0;
+        for (Expense expense : event.getExpenses()) {
+            if (expense.getType() == null) {
+                double amount = expense.getAmount();
+                try {
+                    amount = converter.convert(expense.getCurrency(), userConfig.getCurrency(),
+                            amount, expense.getDate().toInstant());
+                    costExpensesNoTag += amount;
+                } catch (IOException e) {
+                    handleCurrencyError(e);
+                }
+            }
+        }
+        return costExpensesNoTag;
+    }
+
+    /**
+     * update the no tag slices
+     * @param event the current event
+     * @param totalCost the total cost of the event
+     */
+    private void updateNoTagSlice(Event event, double totalCost) {
+        for (PieChart.Data slice : pc.getData()) {
+            if (slice.getName().contains("No tag")) {
+                double costExpensesNoTag = calculateExpensesNoTag(event);
+                String text = configureNoTag(costExpensesNoTag, totalCost);
+                slice.setName(text);
+                slice.setPieValue(costExpensesNoTag);
+                break;
+            }
+        }
     }
 
     /**
@@ -208,11 +295,13 @@ public class StatisticsCtrl {
      */
     public String configureNoTag(double costExpensesNoTag, double totalCost) {
         double percentage = costExpensesNoTag / totalCost * 100;
+        String preferedCurrency = userConfig.getCurrency();
+        String form = getCurrencySymbol(costExpensesNoTag, preferedCurrency);
         String formattedPercentage = String.format("%.2f", percentage);
         String temp = "";
         temp += "No tag\n";
         temp += formattedPercentage + "%";
-        temp += " (" + costExpensesNoTag + ")";
+        temp += " (" + form + ")";
         return temp;
     }
 
@@ -225,9 +314,25 @@ public class StatisticsCtrl {
     public double getAmount(Event event, Tag tag) {
         double rez = 0;
         for (Expense exp : event.getExpenses()) {
+            double amount = exp.getAmount();
+            try {
+                if(!userConfig.getCurrency().equals("NONE")) {
+                    amount = converter.convert(exp.getCurrency(), userConfig.getCurrency(),
+                            amount, exp.getDate().toInstant());
+                }
+
+            } catch (ConnectException e) {
+                mainCtrl.handleServerNotFound();
+            } catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR,
+                        languageConf.get("Currency.IOError"));
+                alert.setHeaderText(languageConf.get("unexpectedError"));
+                alert.showAndWait();
+            }
+
             if (tag != null && exp.getType() != null) {
                 if (exp.getType().equals(tag)) {
-                    rez += exp.getAmount();
+                    rez += amount;
                 }
             }
         }
@@ -249,5 +354,32 @@ public class StatisticsCtrl {
         int blue = Integer.parseInt(hexCode.substring(5, 7), 16);
 
         return Color.rgb(red, green, blue);
+    }
+
+    /**
+     * handle the currency error
+     * @param e the exception
+     */
+    private void handleCurrencyError(Exception e) {
+        if (e instanceof ConnectException) {
+            mainCtrl.handleServerNotFound();
+        } else if (e instanceof IOException) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, languageConf.get("Currency.IOError"));
+            alert.setHeaderText(languageConf.get("unexpectedError"));
+            alert.showAndWait();
+        }
+    }
+
+    /**
+     * get the formatted amount plus the symbol
+     * @param amount the amount
+     * @param currency the currency
+     * @return the formatted string
+     */
+    public String getCurrencySymbol(double amount, String currency) {
+        NumberFormat formater = NumberFormat.getCurrencyInstance(Locale.getDefault());
+        formater.setMaximumFractionDigits(2);
+        formater.setCurrency(Currency.getInstance(currency));
+        return formater.format(amount);
     }
 }
