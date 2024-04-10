18e195ec057be79ea1720168a441ff085bc2892b
Kajus Kuncaitis <K.Kuncaitis@student.tudelft.nl>
2024-04-11 00:32:39 +0200

    Added BIC to participants and participant editor

--- client/src/main/java/client/scenes/EditParticipantsCtrl.java
+++ client/src/main/java/client/scenes/EditParticipantsCtrl.java
@@ -10,12 +10,8 @@ import commons.Event;
 import commons.Participant;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
 import javafx.scene.text.Text;
 
 import java.net.ConnectException;
@@ -132,6 +128,7 @@ public class EditParticipantsCtrl {
                 emailField.setText(p.getEmailAddress());
                 beneficiaryField.setText(p.getBeneficiary());
                 ibanField.setText(p.getAccountNumber());
+                bicField.setText(p.getBic());
             }
         });
 
@@ -228,7 +225,7 @@ public class EditParticipantsCtrl {
                 informNameExists();
                 return;
             }
-            Participant newP = new Participant(name, email, beneficiary, iban);
+            Participant newP = new Participant(name, email, beneficiary, iban, bic);
 
             try {
                 server.createParticipant(event.getId(), newP);
@@ -247,6 +244,7 @@ public class EditParticipantsCtrl {
             currP.setEmailAddress(email);
             currP.setBeneficiary(beneficiary);
             currP.setAccountNumber(iban);
+            currP.setBic(bic);
             try {
                 server.updateParticipant(event.getId(), currP);
             } catch (ConnectException e) {
